c4d30e92ebdf686d85de6a2732a7991cc7b5ee05
Brandon Betts and Craig Wann <brandon@>
2018-06-14 13:26:35 -0700

    add nothing

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,37 @@
 // business
-
-function getRandomInt() {
-  return Math.floor(Math.random() * 6) + 1;
-}
-
-function Player(name) {
-  this.name = name
+function Player(turn) {
+  this.name
   this.score = 0
+  this.rollScoreTemp = 0
+  this.rollScoreTotal = 0
+  this.turn = turn
 }
 
-Player.prototype.totalScore = function(addition) {
-  var score = []
-  score.push(this.score += addition)
+var rollDie = function() {
+  Math.floor(Math.random() * 6) + 1
 }
 
-
+// Player.prototype.onHold = function() {
+//
+// }
+// var numbers = [10, 20, 30, 40] // sums to 100
+// var sum = 0;
+// for (var i = 0; i < numbers.length; i++) {
+//   sum += numbers[i]
+// }
 
 // user
 $(document).ready(function() {
-  $('button').click(function(event) {
+
+  var playerOne = new Player(true);
+  var playerTwo = new Player(false);
+
+  $('#roll').click(function(event) {
     event.preventDefault();
 
-  var player = new Player();
-  var addition = getRandomInt();
-  var totalScore = player.totalScore(addition);
+    playerOne.rollDie();
+    playerOne.scoreTotal();
 
-
-  console.log(totalScore);
-
-  })
-})
+    $('#score1').text(playerOne.scoreTotal());
+  });
+});
